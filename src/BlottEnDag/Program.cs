c071ba9c07e3b68f94150d149701a0aac6bee971
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-11-23 19:32:20 +0100

    -initial commit

---
+++ src/BlottEnDag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlottEnDag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
