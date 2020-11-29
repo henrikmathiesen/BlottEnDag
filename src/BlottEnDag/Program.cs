ffba2ecec93c260c1203dbd5d238814b3335d569
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-11-29 15:44:53 +0100

    -oneDay work in progress

--- src/BlottEnDag/Program.cs
+++ src/BlottEnDag/Program.cs
@@ -6,7 +6,24 @@ namespace BlottEnDag
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Say something");
+
+            // var input = Console.ReadLine();
+            // var today = DateTime.Now.ToShortDateString();
+
+            // var oneDay = new OneDay(today);
+
+            // Console.WriteLine(input);
+
+            var oneDay = new OneDay(DateTime.Now.ToShortDateString());
+
+            for (int i = 0; i < oneDay.GetQuestionCount(); i++)
+            {
+                Console.WriteLine(oneDay.GetQuestionAndAnswer(i));
+            }
+
+
+            
         }
     }
 }
