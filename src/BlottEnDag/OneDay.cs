c24d009fc59ac76b17a97856c91af00bb7bde6ee
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-11-29 15:55:30 +0100

    -static list

--- src/BlottEnDag/OneDay.cs
+++ src/BlottEnDag/OneDay.cs
@@ -1,27 +1,31 @@
 
 using System.Collections.Generic;
 
-namespace BlottEnDag {
+namespace BlottEnDag
+{
 
-    public class OneDay {
+    public class OneDay
+    {
 
         private string date;
-        private List<string> questions = new List<string>();
-        private List<bool> answers = new List<bool>();
+        private List<string> questions;
+        private List<bool> answers;
 
-        public OneDay(string date){
+        public OneDay(string date)
+        {
             this.date = date;
-            this.setQuestions();
-            this.setAnswers();
+            this.questions = QuestionsAndAnswers.questions;
+            this.answers = QuestionsAndAnswers.answers;
         }
 
         public int GetQuestionCount()
         {
-            if(this.questions.Count == this.answers.Count) 
+            if (this.questions.Count == this.answers.Count)
             {
                 return this.questions.Count;
             }
-            else {
+            else
+            {
                 return 0;
             }
         }
@@ -31,40 +35,14 @@ namespace BlottEnDag {
             return this.questions[nr];
         }
 
-        public void SetAnswer(int nr, bool answer) 
-        {
-            this.answers[nr] = answer;
-        }
-
         public string GetQuestionAndAnswer(int nr)
         {
             return $"{this.questions[nr]} | {this.answers[nr]}";
         }
 
-        private void setQuestions()
+        public void SetAnswer(int nr, bool answer)
         {
-            this.questions.Add("Tränade du CrossFit?");
-            this.questions.Add("Gick du till lokala gymmet?");
-            this.questions.Add("Tränade du i hemmagymmet?");
-            this.questions.Add("Över 7000 steg på stegräknaren?");
-            this.questions.Add("Under 3 timmar i fåtölj?");
-            this.questions.Add("Ätit en frukt?");
-            this.questions.Add("Ätit grönsaker?");
-            this.questions.Add("Ätit efterrätt?");
-            this.questions.Add("Ätit snacks?");
-        }
-
-        private void setAnswers()
-        {
-            this.answers.Add(false);
-            this.answers.Add(false);
-            this.answers.Add(false);
-            this.answers.Add(false);
-            this.answers.Add(false);
-            this.answers.Add(false);
-            this.answers.Add(false);
-            this.answers.Add(false);
-            this.answers.Add(false);
+            this.answers[nr] = answer;
         }
 
     }
---
+++ src/BlottEnDag/QuestionsAndAnswers.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace BlottEnDag
+{
+
+    static class QuestionsAndAnswers
+    {
+
+        public static List<string> questions = new List<string>()
+        {
+            "Tränade du CrossFit?",
+            "Gick du till lokala gymmet?",
+            "Tränade du i hemmagymmet?",
+            "Över 7000 steg på stegräknaren?",
+            "Under 3 timmar i fåtölj?",
+            "Ätit en frukt?",
+            "Ätit grönsaker?",
+            "Ätit efterrätt?",
+            "Ätit snacks?",
+        };
+
+        public static List<bool> answers = new List<bool>()
+        {
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        };
+
+    }
+
+}
