e094a8e55ce417b278d7bc24f60f79f883e309e5
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-12-22 11:40:37 +0100

    -test project -reference BlottEnDag project -test skeleton

---
+++ test/BlottEnDag.Tests/OneDayTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace BlottEnDag.Tests
+{
+    public class OneDayTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var oneDay = new OneDay(DateTime.Now);
+            
+            // Act
+            
+
+            // Assert
+            
+        }
+    }
+}
