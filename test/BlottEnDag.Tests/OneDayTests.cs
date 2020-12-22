e5ef8b63e9e6ccaa4b9f97c8ad8c918375594e6c
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-12-22 13:50:26 +0100

    -testing using question count to set and get answers

--- test/BlottEnDag.Tests/OneDayTests.cs
+++ test/BlottEnDag.Tests/OneDayTests.cs
@@ -6,16 +6,36 @@ namespace BlottEnDag.Tests
     public class OneDayTests
     {
         [Fact]
-        public void Test1()
+        public void UseTheQuestionCountToSetAnswersAndGetTheAnswers()
         {
-            // Arrange
             var oneDay = new OneDay(DateTime.Now);
-            
-            // Act
-            
 
-            // Assert
-            
+            for (int i = 0; i < oneDay.GetQuestionCount(); i++)
+            {
+                oneDay.SetAnswer(i, GetIsEvenNumber(i) ? true : false);
+            }
+
+            for (int i = 0; i < oneDay.GetQuestionCount(); i++)
+            {
+                var answer = oneDay.GetQuestionAndAnswer(i);
+                
+                var actual = answer.Split(" | ")[1].Trim();
+                var expected = GetIsEvenNumber(i) ? "Ja" : "Nej";
+
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        private bool GetIsEvenNumber(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
