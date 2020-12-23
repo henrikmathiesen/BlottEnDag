5fc6d08341f275261888d4ab2e3a01a371e013d9
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-12-23 20:03:09 +0100

    -todo comment

--- src/BlottEnDag/Program.cs
+++ src/BlottEnDag/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 
+/* 
+
+    TODO: store date in DB
+    https://docs.microsoft.com/en-us/dotnet/standard/base-types/standard-date-and-time-format-strings
+    string search: 6/15/2009 1:45:30 PM (Utc) --> 2009-06-15T13:45:30.0000000Z
+
+*/
+
 namespace BlottEnDag
 {
     class Program
